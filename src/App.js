d9c02679669c1a1cfd73ddfae5f39b9913e5ba65
asarmie <asarmiento99@gmail.com>
2022-02-27 16:56:44 -0500

    added github repo-url

--- src/App.js
+++ src/App.js
@@ -4,6 +4,13 @@ export default function App() {
   return (
     <div className="App">
       <h1>Weather App</h1>
+
+      <footer>
+        <a href="https://elated-spence-89c00b.netlify.app/" target="_blank">
+          Open sourced code
+        </a>
+        , by Andrea Sarmiento
+      </footer>
     </div>
   );
 }
