3c3e546a76e7b6acc711f2d0b92f5e62d15f7111
asarmie <asarmiento99@gmail.com>
2022-02-27 17:30:52 -0500

    added bootstrap to project

--- src/App.js
+++ src/App.js
@@ -1,16 +1,23 @@
+import React from "react";
 import "./App.css";
 
 export default function App() {
   return (
     <div className="App">
-      <h1>Weather App</h1>
+      <div className="container">
+        <h1>Weather App</h1>
 
-      <footer>
-        <a href="https://elated-spence-89c00b.netlify.app/" target="_blank">
-          Open sourced code
-        </a>
-        , by Andrea Sarmiento
-      </footer>
+        <footer>
+          <a
+            href="https://elated-spence-89c00b.netlify.app/"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Open sourced code
+          </a>
+          , by Andrea Sarmiento
+        </footer>
+      </div>
     </div>
   );
 }
